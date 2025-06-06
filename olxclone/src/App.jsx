0375405a8d25657b1ad769ecda470d6f9555e211
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-06 06:15:49 +0530

    fix app.js

--- olxclone/src/App.jsx
+++ olxclone/src/App.jsx
@@ -1,49 +1,52 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/login/Login";
 import SignUp from "./pages/Signup/SignUp";
 import Homepage from "./pages/home/Homepage";
 import ProductPage from "./pages/productpage/ProductPage";
 import AddProductPage from "./pages/addproductpage/AddProductPage";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        
-        <Route path="/" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public */}
+          <Route path="/" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
 
-         
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Homepage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/product/:id"
-          element={
-            <ProtectedRoute>
-              <ProductPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/addproduct"
-          element={
-            <ProtectedRoute>
-              <AddProductPage />
-            </ProtectedRoute>
-          }
-        />
-        
-      </Routes>
-    </Router>
+          {/* Protected */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/product/:id"
+            element={
+              <ProtectedRoute>
+                <ProductPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addproduct"
+            element={
+              <ProtectedRoute>
+                <AddProductPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
