c4d9d8df913c6a8c70890dd0ab546cd77190ac78
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-06 06:05:23 +0530

    fix proctectd

--- olxclone/src/App.jsx
+++ olxclone/src/App.jsx
@@ -1,27 +1,57 @@
+import React from "react";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/Signup/SignUp";
 import Homepage from "./pages/home/Homepage";
 import ProductPage from "./pages/productpage/ProductPage";
- 
 import AddProductPage from "./pages/addproductpage/AddProductPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { MyContextProvider } from "./context/MyContext";
+import Player from "./pages/player/Player"; // Assuming you have this
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/addproduct" element={<AddProductPage />} />
-         
 
+        {/* Protected routes */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <ProtectedRoute>
+              <ProductPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/addproduct"
+          element={
+            <ProtectedRoute>
+              <AddProductPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/player/:id"
+          element={
+            <ProtectedRoute>
+              <Player />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-
 }
 
-export default App
+export default App;
